1515958637eba7938f1da6acf69dabde2ba8a869
Parker <ygfert@gmail.com>
2020-09-02 14:00:29 +0800

    在 db:seed 裡面建立 50 筆 books 和 users, 並建立之間的關聯

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook]
+         :omniauthable, omniauth_providers: [:facebook]
 
   validates :email, presence: true, uniqueness: true
 
   has_many :book_authors
-  has_many :pub_books, through: :book_authors, source: :books
+  has_many :pub_books, through: :book_authors, source: :book
 
   has_many :book_users
-  has_many :bought_books, through: :book_users, source: :books
+  has_many :bought_books, through: :book_users, source: :book
 
   def self.from_omniauth(auth)
     # Case 1: Find existing user by facebook uid
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root "home#index"
 
-  resources :books, only: [:show, :new, :create]
+  resources :books, only: [:index, :show, :new, :create]
   
 end
