e8dde0293462834a7fdac03ce3c8e2f117ec888e
Parker-ygfert <52908047+Parker-ygfert@users.noreply.github.com>
2020-09-02 20:39:12 +0800

    在 db:seed 裡面建立 50 筆 books 和 users, 並建立之間的關聯 (#13)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,9 @@
 class BooksController < ApplicationController
 
+  def index
+    @books = Book.all
+  end
+  
   def show
     @book = Book.find(params[:id])
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, filter_html: false, autolink: true, tables: true)
--- app/models/book.rb
+++ app/models/book.rb
@@ -5,10 +5,10 @@ class Book < ApplicationRecord
   has_many :tags, through: :taggings
   
   has_many :book_authors
-  has_many :authors, through: :book_authors, source: :users
+  has_many :authors, through: :book_authors, source: :user
 
   has_many :book_users
-  has_many :readers, through: :book_users, source: :users
+  has_many :readers, through: :book_users, source: :user
 
   has_one :order_items
   
