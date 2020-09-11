fe5c7af23a722cab0fadc1c44cbaad90d98ae5e8
Ted Chen <teddy70642000@gmail.com>
2020-09-11 14:33:42 +0800

    import 語言檢查的 js 程式
    1- import codemirror 黑色主題
    
    2- import 我們熟悉的語言 highlihgt js script

--- app/javascript/stylesheets/text_editor/index.js
+++ app/javascript/stylesheets/text_editor/index.js
@@ -1,6 +1,16 @@
 import CodeMirror from "codemirror/lib/codemirror.js" // 引入 codemirror 套件 
 import "codemirror/lib/codemirror.css"
-
+import "codemirror/theme/abcdef"                      // 編輯器的主題，黑底的主題
+import "codemirror/mode/markdown/markdown"            // markdown 語法 hightlight 
+import "codemirror/mode/ruby/ruby"
+import "codemirror/mode/javascript/javascript"
+import "codemirror/mode/sql/sql"
+import "codemirror/mode/powershell/powershell"
+import "codemirror/mode/shell/shell"
+import "codemirror/mode/xml/xml"
+import "codemirror/mode/css/css"
+import "codemirror/mode/sass/sass"
+// import 這麼多是因為在 markdown 中打的 code 需要對應的 js 程式去掃 才會 highlight
 window.addEventListener('DOMContentLoaded',()=>{
 
   let myCodeMirror //使mycodemirror變成變數
