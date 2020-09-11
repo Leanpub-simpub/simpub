307fd651a60096f58107cf0f9f0ac679ce13f831
Parker <ygfert@gmail.com>
2020-09-11 11:02:05 +0800

    更改購物車的跳轉頁面

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,8 +3,6 @@ class CartsController < ApplicationController
   def add
     current_cart.add_item(params[:id]) 
     session[Cart::SessionKey] = current_cart.serialize
-
-    # redirect_to books_path, notice: "已加入購物車" 
   end
 
   def destroy
