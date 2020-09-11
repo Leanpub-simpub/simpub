5043743982d5fdc258a883e4d00684be5e678ecb
Andrew <jrnalts@gmail.com>
2020-09-11 17:56:17 +0800

    tailwindcss 安裝完成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,7 @@ require("channels");
 
 // require("trix")
 // require("@rails/actiontext")
-require("../stylesheets") 
+require("../stylesheets");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -18,4 +18,5 @@ require("../stylesheets")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-import "bootstrap";import "../css/application.css"
+import "bootstrap";
+import "../css/application.css";
