ea286ae0c0927f5bba196cfa3f4896c68dc6530a
Parker <ygfert@gmail.com>
2020-09-12 15:33:59 +0800

    上傳頭像及封面時顯示預覽圖片

---
+++ app/javascript/packs/image_preview.js
@@ -0,0 +1,23 @@
+document.addEventListener("turbolinks:load", () => {
+  // 個人資料編輯頁面 頭像預覽
+  if (document.querySelector("#avatar-input")) {
+    const avatarImage = document.querySelector("#avatar-preview").firstElementChild;
+    const avatarInput = document.querySelector("#avatar-input");
+    
+    avatarInput.addEventListener("change", (e) => {
+      avatarImage.src = URL.createObjectURL(e.target.files[0]);
+    });
+  }
+  
+  
+  // 編輯書籍資料 封面預覽
+  if (document.querySelector("#cover-input")) {
+    const coverImage = document.querySelector("#cover-preview").firstElementChild;
+    const coverInput = document.querySelector("#cover-input");
+
+    coverInput.addEventListener("change", (e) => {
+      console.log(e.target);
+      coverImage.src = URL.createObjectURL(e.target.files[0])
+    });
+  }
+});
--- app/javascript/packs/payment.js
+++ app/javascript/packs/payment.js
@@ -1,15 +1,6 @@
-// import "@braintree"
-// import "braintree-web";
-// import braintreeDefault, * as braintree from "braintree-web";
 import braintreeDropIn from "braintree-web-drop-in";
 
-// console.log({ braintreeDefault, braintree, braintreeDropInDefault, braintreeDropIn })
-// import "braintree-web-drop-in/dist/browser/dropin";
-// import { dropin } from "braintree-web-drop-in/dist/browser/dropin.js";
-// import { dropin } from "braintree-web";
-// import { dropin } from "braintree-web-drop-in";
-
-document.addEventListener("turbolinks:load", function() {
+document.addEventListener("turbolinks:load", () => {
 
   if (document.querySelector("#submit-button")) {
   
