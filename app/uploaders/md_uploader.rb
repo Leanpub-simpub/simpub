6c6671a2ea6bd6898066df03c6571a07470e34b7
jean <macyjeanma@gmail.com>
2020-09-13 17:08:46 +0800

    切library頁面

--- app/uploaders/md_uploader.rb
+++ app/uploaders/md_uploader.rb
@@ -1,6 +1,6 @@
 class MdUploader < ApplicationUploader
   plugin :pretty_location, identifier: :title
   Attacher.validate do
-    validate_mime_type_inclusion ['file/pdf']
+    validate_mime_type_inclusion ['file.']
   end
 end
