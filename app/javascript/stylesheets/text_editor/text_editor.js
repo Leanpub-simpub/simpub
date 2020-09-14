6afd8aa259f61c9db475b1f39814a3fbb54120c4
Ted Chen <teddy70642000@gmail.com>
2020-09-14 17:43:14 +0800

    修正 md to html 的 code 沒有行號

--- app/javascript/stylesheets/text_editor/text_editor.js
+++ app/javascript/stylesheets/text_editor/text_editor.js
@@ -46,9 +46,19 @@ window.addEventListener('turbolinks:load',()=>{
           highlight: function (str, lang) {
             if (lang && hljs.getLanguage(lang)) {
               try {
+                // 得到经过highlight.js之后的html代码
+                const preCode = hljs.highlight(lang, str, true).value
+                // 以换行进行分割
+                const lines = preCode.split(/\n/).slice(0, -1)
+                // 添加自定义行号
+                let html = lines.map((item, index) => {
+                  return '<li><span class="line-num" data-line="' + (index + 1) + '"></span>' + item + '</li>'
+                }).join('')
+                html = '<ol>' + html + '</ol>'
+
                 return '<pre class="hljs"><code>' +
-                       hljs.highlight(lang, str, true).value +
-                       '</code></pre>';
+                  html +
+                  '</code></pre>'
               } catch (__) {}
             }
             return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
