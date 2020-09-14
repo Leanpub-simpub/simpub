7e6975daa5878c9dbbffe3a97ea193249716b8c0
Andrew <jrnalts@gmail.com>
2020-09-14 12:26:26 +0800

    視覺外觀調整; 商品搜尋頁設定間距

--- app/javascript/stylesheets/index.js
+++ app/javascript/stylesheets/index.js
@@ -1,7 +1,7 @@
-import "bootstrap/dist/css/bootstrap"
-import "@fortawesome/fontawesome-free/css/all"
+import "bootstrap/dist/css/bootstrap";
+import "@fortawesome/fontawesome-free/css/all";
 import "./actiontext.scss";
-import "./avatar.scss"
-import "./text_editor"
-require('./layout')
-
+import "./avatar.scss";
+import "./home.scss";
+import "./text_editor";
+require("./layout");
