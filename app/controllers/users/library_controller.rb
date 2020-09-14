2ebb216ca813d85e64a239ad8dc6a8f69055b491
jean <macyjeanma@gmail.com>
2020-09-14 10:16:07 +0800

    切library頁面

--- app/controllers/users/library_controller.rb
+++ app/controllers/users/library_controller.rb
@@ -1,22 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 class Users::LibraryController < ApplicationController
 
   def show
     @books = current_user.bought_books
   end
-=======
-class Users::LibarayController < ApplicationController
-=======
-class Users::LibraryController < ApplicationController
->>>>>>> 建立libaray 完成
-
-  def show
-    @books = current_user.bought_books
-  end
-<<<<<<< HEAD
-
->>>>>>> 建立libaray
-=======
->>>>>>> 建立libaray 完成
 end
