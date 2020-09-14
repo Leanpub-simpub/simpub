9c2128a0e6f66c166804afee2cb1198ab8bed38e
Ted Chen <teddy70642000@gmail.com>
2020-09-14 22:06:54 +0800

    對 webpack js 檔下判斷，阻止console 錯誤出現

--- app/javascript/stylesheets/text_editor/syn_scroll.js
+++ app/javascript/stylesheets/text_editor/syn_scroll.js
@@ -1,26 +1,24 @@
 window.addEventListener('turbolinks:load',()=>{
-  let codemirror = document.querySelector('.CodeMirror-scroll')
-  let markdownIt = document.querySelector('#targetDiv')
-
-  codemirror.addEventListener('mousewheel',()=>{
+  if(document.querySelector('#targetDiv')){
+    let codemirror = document.querySelector('.CodeMirror-scroll')
+    let markdownIt = document.querySelector('#targetDiv')
+  
+    codemirror.addEventListener('mousewheel',()=>{
       let leftHight = codemirror.scrollHeight
-    let currentY = codemirror.scrollTop
-    let currentX = codemirror.scrollLeft
-    let rightHight = markdownIt.scrollHeight
-    let rightMoveTo = (currentY/leftHight) * rightHight
-    markdownIt.scrollTo(currentX,rightMoveTo)
-    
-  })
-
-  markdownIt.addEventListener('mousewheel',()=>{
-    
+      let currentY = codemirror.scrollTop
+      let currentX = codemirror.scrollLeft
       let rightHight = markdownIt.scrollHeight
-      let currentY = markdownIt.scrollTop
-      let currentX = markdownIt.scrollLeft
-      let leftHight = codemirror.scrollHeight
-      let leftMoveTo = (currentY/rightHight)* leftHight
-      codemirror.scrollTo(currentX,leftMoveTo)
-    
-  })
-
+      let rightMoveTo = (currentY/leftHight) * rightHight
+      markdownIt.scrollTo(currentX,rightMoveTo)  
+    })
+  
+    markdownIt.addEventListener('mousewheel',()=>{
+        let rightHight = markdownIt.scrollHeight
+        let currentY = markdownIt.scrollTop
+        let currentX = markdownIt.scrollLeft
+        let leftHight = codemirror.scrollHeight
+        let leftMoveTo = (currentY/rightHight)* leftHight
+        codemirror.scrollTo(currentX,leftMoveTo)
+    })
+  }
 })
