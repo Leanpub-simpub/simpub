947ee8c4b7078db0d3e474dc8a5bd0e1a1d62133
Andrew <jrnalts@gmail.com>
2020-09-15 13:12:41 +0800

    移除 tailwindcss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,6 @@ require("channels");
 
 // require("trix")
 // require("@rails/actiontext")
-require("../stylesheets");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -20,5 +19,4 @@ const images = require.context("../images", true);
 const imagePath = (name) => images(name, true);
 
 import "bootstrap";
-import "../css/application.css";
-
+require("../stylesheets");
