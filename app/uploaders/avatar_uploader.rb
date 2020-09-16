f767bebcded14f699fe08aee82875f08eac52e24
Parker <ygfert@gmail.com>
2020-09-16 19:11:04 +0800

    新增 remote_url plugin

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,5 +1,7 @@
 class AvatarUploader < ApplicationUploader
   plugin :pretty_location, identifier: :username
+  plugin :remote_url, max_size: 20*1024*1024
+
   Attacher.validate do
     validate_mime_type_inclusion ['image/jpg', 'image/jpeg', 'image/png']
   end
