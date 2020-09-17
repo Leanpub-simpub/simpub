999675bbb9aff91d9647a0e5f2cdd3e0c2a7e133
jean <macyjeanma@gmail.com>
2020-09-17 17:22:29 +0800

    修正user更新密碼驗證

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,20 +3,18 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters, only: [:create, :update]
 
-  # def create
-  #   @user = User.new(configure_permitted_parameters)
-    
-  #   if @user.save
-  #     WelcomeMailer.with(user: @user).welcome_email.deliver_now
-  #     redirect_to '/', notice: 'User was successfully created.'
-  #   end
-  # end
+
  
   private
   # 在 devise 中加入 name & username 欄位
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :name, :username, :about, :password])
+    params.require(:user).permit(:first_name, :last_name, :address)
+  end
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
   end
 
   # update 的時候不需要驗證密碼
