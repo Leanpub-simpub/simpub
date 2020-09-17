e2ce62e405495a9f8ec91686247f1529fdd11e52
Andrew <jrnalts@gmail.com>
2020-09-17 12:41:56 +0800

    視覺外觀調整: book/index, home

--- app/javascript/stylesheets/index.js
+++ app/javascript/stylesheets/index.js
@@ -2,14 +2,13 @@ import "bootstrap/dist/css/bootstrap";
 import "@fortawesome/fontawesome-free/css/all";
 import "./actiontext.scss";
 
-import "./avatar.scss"
-import "./text_editor"
-import "./book_search.scss"
+import "./avatar.scss";
+import "./text_editor";
+import "./book_search.scss";
 // require('./layout')
 
 import "./avatar.scss";
-import "./home.scss";
 import "./text_editor";
 import "./books";
-import "./layout";
-
+import "./home";
+import "./shared";
