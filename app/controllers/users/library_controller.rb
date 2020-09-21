970d1db9b746a1249e04661cf4c618e475fef66a
Jean Ma <67670050+macyjeanma@users.noreply.github.com>
2020-09-21 21:58:03 +0800

    Library (#51)
    * 修改library樣式
    
    * 修正user更新密碼驗證
    
    * 建立library info連結
    
    * 修正library info連結
    
    * 修正routes錯誤
    
    * 修正routes 錯誤

--- app/controllers/users/library_controller.rb
+++ app/controllers/users/library_controller.rb
@@ -2,6 +2,7 @@ class Users::LibraryController < ApplicationController
 
   def show
     @books = current_user.bought_books
+    @show_book = Book.find_by(id: params[:format])
   end
 
   
--- app/javascript/books/index.js
+++ app/javascript/books/index.js
@@ -1,2 +1,3 @@
-import "./tag_list.js"
-import "./slider.js"
+import "./tag_list.js";
+import "./slider.js";
+import "./library_info.js";
