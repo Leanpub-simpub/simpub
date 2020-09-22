d768b08857210942452a1ccb580eb29f989ba622
Chen-Jhen-Ting <67670844+Chen-Jhen-Ting@users.noreply.github.com>
2020-09-22 10:29:50 +0800

    Frontend fix2 (#58)
    * test
    
    * fix
    
    * 書本上架頁面 位置調整到水平與垂直置中，文字白色
    
    * book info edit page 表單水平置中 文字白色
    
    * library  加上線上閱讀的連結，連到暫時閱讀畫面，暫時文字白色
    
    * library  加上線上閱讀的連結，連到暫時閱讀畫面，暫時文字白色
    
    * 購物車 撐開高度，讓，讓footer 到底部
    
    * edit profile 表單顏色調成淺米色，避免太刺眼

--- app/javascript/books/library_info.js
+++ app/javascript/books/library_info.js
@@ -1,5 +1,5 @@
-window.addEventListener("turbolinks:load", function () {
-  document.querySelector(".book-title").addEventListener("click", function () {
-    console.log(1111);
-  });
-});
+// window.addEventListener("turbolinks:load", function () {
+//   document.querySelector(".book-title").addEventListener("click", function () {
+//     console.log(1111);
+//   });
+// });
