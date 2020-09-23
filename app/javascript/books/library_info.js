f54c08a2f5f65488e6710dd790a276ac737d9c3e
jean <macyjeanma@gmail.com>
2020-09-23 19:01:45 +0800

    加入 library info 連結

--- app/javascript/books/library_info.js
+++ app/javascript/books/library_info.js
@@ -1,5 +1,18 @@
-// window.addEventListener("turbolinks:load", function () {
-//   document.querySelector(".book-title").addEventListener("click", function () {
-//     console.log(1111);
-//   });
-// });
+window.addEventListener("turbolinks:load", function () {
+  if (document.querySelector("section.library")) {
+    const bookInfo = document.querySelector("section.library")
+      .firstElementChild;
+    bookInfo.addEventListener("click", (e) => {
+      if (!e.target.classList.contains("cover-link")) {
+        return;
+      }
+
+      // TODO
+      e.target.appendChild();
+    });
+  }
+
+  // document.querySelectorAll(".cover").addEventListener("click", function () {
+  //   console.log(1111);
+  // });
+});
