6d0fc846775ea7411acd4f0bec88bc4ef99b9a08
Ted Chen <teddy70642000@gmail.com>
2020-09-24 18:03:58 +0800

    前端限制 新增 chapter and section 時 不可以重複，並給予訊息提示

--- app/javascript/stylesheets/text_editor/add_chapter_and_section.js
+++ app/javascript/stylesheets/text_editor/add_chapter_and_section.js
@@ -17,11 +17,10 @@ window.addEventListener('turbolinks:load',()=>{
     let chapterList = document.querySelector('.chapter_list')
     let addChapter =document.querySelector('#addChapter')
     let chapterform = document.querySelector('.chapterCreate')
-    let addSection = document.querySelectorAll('.addsection')
     let sectionform = document.querySelector('.sectionCreate')
     let sectionInsetTarget // 之後新增 section 要當作位置參照
-
- 
+    let chapterErr = document.querySelector('#chapterErr')
+    let sectionErr = document.querySelector('#sectionErr')
 
     // add-chapter-form control
     addChapter.addEventListener('click',(e)=>{
@@ -33,15 +32,19 @@ window.addEventListener('turbolinks:load',()=>{
     let chapterAgree = document.querySelector('#chapterAgreeBtn')
     let chapterInput = document.querySelector('[name="chapter"]')
     chapterAgree.addEventListener('click',()=>{
+      let chapterName = []
+      document.querySelectorAll('.chapter').forEach(chapter =>{
+        chapterName.push(chapter.textContent)
+      })
       if(chapterInput.value === "" ){
         // 如果沒填 chapter 名稱
         chapterInput.style.border = 'red 3px solid'
         chapterInput.placeholder = 'Chapter name is blank'
-        setTimeout(function(){
-          chapterInput.style.border = 'black 1px solid'
-        chapterInput.placeholder = ''
-        },5000)
-      }else{
+      }else if(!chapterName.includes(chapterInput.textContent)){
+        chapterInput.style.border = 'red 3px solid'
+        chapterErr.textContent = 'Chapter name is repeated'
+      }
+      else{
         // 有填寫 chapter 名稱即可送出
         chapterform.submit()
         chapterform.classList.add('x')
@@ -52,6 +55,7 @@ window.addEventListener('turbolinks:load',()=>{
         chapterDOM.querySelector('.chapter').dataset.order = order
         chapterList.insertBefore(chapterDOM,addChapter)
         setTimeout(function(){
+          chapterErr.textContent =""
           chapterInput.value = ""
         },1)
       }
@@ -84,6 +88,10 @@ window.addEventListener('turbolinks:load',()=>{
     let sectionInput = document.querySelector('[name="section"]')
     
     sectionAgree.addEventListener('click',()=>{
+      let sectionName=[]
+      sectionInsetTarget.querySelectorAll('.section').forEach(section=>{
+        sectionName.push(section.textContent)
+      })
       if(sectionInput.value === "" ){
         // 如果沒填 section 名稱
         sectionInput.style.border = 'red 3px solid'
@@ -92,7 +100,11 @@ window.addEventListener('turbolinks:load',()=>{
           sectionInput.style.border = 'black 1px solid'
           sectionInput.placeholder = ''
         },5000)
-      }else{
+      }else if(!sectionName.includes(sectionInput.value)){
+        sectionInput.style.border = 'red 3px solid'
+        sectionErr.textContent = 'Section name is repeat'
+      }
+      else{
         // 有填寫 section 名稱即可送出
         sectionform.submit()
         sectionform.classList.add('x')
@@ -107,7 +119,8 @@ window.addEventListener('turbolinks:load',()=>{
         // addSectionBtn.previousSibling.previousSibling.insertAdjacentElement("afterend",sectionDOM)
         setTimeout(function(){
           sectionInput.value = ""
-        },5000)
+          sectionErr.textContent = ""
+        },10)
       }
     })
 
