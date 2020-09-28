73a861f660e85c3071460991dd49647841e8a0fe
Andrew <jrnalts@gmail.com>
2020-09-28 12:52:25 +0800

    調整全站 CSS 設定 調整 navbar 樣式

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -19,7 +19,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def google_oauth2
     @user = User.from_omniauth(request.env["omniauth.auth"], current_user)
-
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
       sign_in_and_redirect @user, event: :authentication
