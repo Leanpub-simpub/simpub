7427145706fc005b0afd4c6ed3e51afecf6d6724
Parker <ygfert@gmail.com>
2020-09-28 16:40:32 +0800

    建立 follower 和 followed 關聯

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,12 @@ class User < ApplicationRecord
 
   has_many :orders
 
+  has_many :followers, foreign_key: :follower_id, class_name: :followship
+  has_many :followers, through: :followed
+  
+  has_many :followed, foreign_key: :followed_id, class_name: :followship
+  has_many :followed, through: :followers
+
   def self.from_omniauth(auth, signed_in_resource = nil)
     identity = Identity.find_for_oauth(auth)
     user = signed_in_resource ? signed_in_resource : identity.user
