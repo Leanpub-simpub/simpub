d8d068b6df00734a06cbfefaebff86f9ce820e43
jean <macyjeanma@gmail.com>
2020-09-29 21:45:12 +0800

    加入 library info 連結完成

--- app/javascript/books/library_info.js
+++ app/javascript/books/library_info.js
@@ -4,13 +4,24 @@ window.addEventListener("turbolinks:load", function () {
 
     bookInfo.addEventListener("click", e => {
       let classes = e.target.classList;
+      
       if (classes.contains("book-cover") || classes.contains("book-title")) {
         let bookKey = e.target.parentElement.dataset.key;
+        let infoCoverBox = document.querySelector(".info-cover-box");
 
-        fetch(`http://localhost:3000/dash_board/library.json?id=${bookKey}`)
+        if (!infoCoverBox.firstElementChild) {
+          let cover = e.target.parentElement.firstElementChild;
+          let infoCover = cover.cloneNode(true);
+          infoCoverBox.appendChild(infoCover);
+        }
+
+        fetch(`/dash_board/library.json?id=${bookKey}`)
         .then((response) => response.json())
         .then(book => {
-          document.querySelector("book-about");
+          document.querySelector(".book-info-title").textContent = book.title
+          document.querySelector(".name").textContent = book.authors[0].name
+          document.querySelector(".name").image = book.authors[0].avatar_data
+          document.querySelector(".Leanpub").href = `/books/${book.id}/read`
 
         });
       };
