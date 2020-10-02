6c0dd0caa4627a5da041f936eeb1ad8bf746d48c
jrnalts <jrnalts@gmail.com>
2020-10-02 11:58:21 +0800

    引入Typed.js

---
+++ app/javascript/packs/typed.js
@@ -0,0 +1,6 @@
+import Typed from "typed.js";
+
+var typed = new Typed(".element", {
+  strings: ["This is a JavaScript library", "This is an ES6 module"],
+  smartBackspace: true, // Default value
+});
