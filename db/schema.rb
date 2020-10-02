50d5a8a98d4d8bc4fcb685460268a55de87b03cd
Parker <ygfert@gmail.com>
2020-10-02 15:44:41 +0800

    Order Model 加入 uuid 欄位

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_160918) do
+ActiveRecord::Schema.define(version: 2020_10_02_070620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(version: 2020_09_29_160918) do
     t.float "total"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.uuid "uuid", null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
