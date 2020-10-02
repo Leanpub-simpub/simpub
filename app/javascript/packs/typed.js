7fc8f30716b79dee526dcb5f56958d87f76bebfa
jrnalts <jrnalts@gmail.com>
2020-10-02 15:06:02 +0800

    typed.js已可使用

--- app/javascript/packs/typed.js
+++ app/javascript/packs/typed.js
@@ -1,6 +1,23 @@
-import Typed from "typed.js";
-
-var typed = new Typed(".element", {
-  strings: ["This is a JavaScript library", "This is an ES6 module"],
-  smartBackspace: true, // Default value
+$(function () {
+  $(".typed").typed({
+    strings: ["Simple to publish. <br> Simple to write."],
+    // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+    stringsElement: null,
+    // typing speed
+    typeSpeed: 80,
+    // time before typing starts
+    startDelay: 1200,
+    // backspacing speed
+    loop: true,
+    // false = infinite
+    loopCount: 1,
+    // show cursor
+    showCursor: false,
+    // character for cursor
+    cursorChar: "|",
+    // attribute to type (null == text)
+    attr: null,
+    // either html or text
+    contentType: "html",
+  });
 });
