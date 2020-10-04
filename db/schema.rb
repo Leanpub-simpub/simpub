cb718d4fc92141a0a984a623402ccb716c071884
Parker <ygfert@gmail.com>
2020-10-04 21:17:36 +0800

    更改 book 的初始出版狀態

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_04_053347) do
+ActiveRecord::Schema.define(version: 2020_10_04_131346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,7 +72,7 @@ ActiveRecord::Schema.define(version: 2020_10_04_053347) do
     t.integer "pages"
     t.integer "words"
     t.integer "completeness"
-    t.string "publish_state", default: "off-shelf"
+    t.string "publish_state", default: "draft"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "cover_data"
