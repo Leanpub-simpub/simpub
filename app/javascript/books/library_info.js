58f581891b5f1a3b0b952710e6e4d7842c0c7af7
Parker <ygfert@gmail.com>
2020-10-06 17:39:37 +0800

    修正 book info 的圖片連動問題

--- app/javascript/books/library_info.js
+++ app/javascript/books/library_info.js
@@ -1,28 +1,37 @@
 window.addEventListener("turbolinks:load", function () {
-  if (document.querySelector("section.library")) {
-    const bookInfo = document.querySelector("section.library").firstElementChild;
+  if (document.querySelector(".book-box")) {
+    const bookBox = document.querySelector(".book-box");
 
-    bookInfo.addEventListener("click", e => {
+    bookBox.addEventListener("click", e => {
       let classes = e.target.classList;
       
       if (classes.contains("book-cover") || classes.contains("book-title")) {
         let bookKey = e.target.parentElement.dataset.key;
         let infoCoverBox = document.querySelector(".info-cover-box");
-
-        if (!infoCoverBox.firstElementChild) {
-          let cover = e.target.parentElement.firstElementChild;
-          let infoCover = cover.cloneNode(true);
-          infoCoverBox.appendChild(infoCover);
-        }
-
+        let bookInfoTitle = document.querySelector(".book-info-title");
+        let bookInfoAvatar = document.querySelector(".book-info-avatar");
+        
         fetch(`/dash_board/library.json?id=${bookKey}`)
         .then((response) => response.json())
         .then(book => {
-          document.querySelector(".book-info-title").textContent = book.title
-          document.querySelector(".name").textContent = book.authors[0].name
-          document.querySelector(".name").image = book.authors[0].avatar_data
-          document.querySelector(".Leanpub").href = `/books/${book.slug}/read`
+          if (bookInfoTitle.textContent == book.title) return;
+          
+            let cover = e.target.parentElement.firstElementChild;
+            let infoCover = cover.cloneNode(true);
+            let avatar = e.target.nextElementSibling;
+            let avatarCover = avatar.cloneNode(true);
+            avatarCover.classList.remove("x");
 
+            infoCoverBox.innerHTML = "";
+            infoCoverBox.appendChild(infoCover);
+
+            bookInfoAvatar.innerHTML = "";
+            bookInfoAvatar.appendChild(avatarCover);
+
+            document.querySelector(".book-info-title").textContent = book.title;
+            document.querySelector(".name").textContent = book.authors[0].name;
+            document.querySelector(".name").image = book.authors[0].avatar_data;
+            document.querySelector(".Leanpub").href = `/books/${book.slug}/read`;
         });
       };
     });
