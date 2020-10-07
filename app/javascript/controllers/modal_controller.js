9e46ad92d4d8ab9d937c6f2a2c576c329e292c0b
Parker <ygfert@gmail.com>
2020-10-07 18:30:05 +0800

    esc 鍵可關閉 modal

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -3,6 +3,17 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   static targets = [ "body" ];
 
+  connect() {
+    // 使用 esc 鍵關閉 modal
+    const body = this.bodyTarget;
+    
+    document.addEventListener("keydown", function(event) {
+      if(event.keyCode === 27){
+        body.classList.add("x");
+      }
+    });
+  }
+  
   close() {
     // 開啟視窗捲動
     document.documentElement.style.overflow = "auto";
