fa2dd9c82b3ea8858c208d6271adb9e1226e18c1
Parker <ygfert@gmail.com>
2020-10-09 22:56:09 +0800

    建立留言功能

--- app/controllers/users/library_controller.rb
+++ app/controllers/users/library_controller.rb
@@ -3,12 +3,29 @@ class Users::LibraryController < ApplicationController
   def show
     @books = current_user.bought_books
     @book_info = Book.find_by(id: params[:id]).as_json(include: :authors)
+    @comment = current_user.comments.new
 
     respond_to do |format|
       format.html
       format.json { render json: @book_info }
     end
   end
+  
+  def comment
+    book = current_user.bought_books.find_by(id: params[:id])
+    comment = book.comments.new(comment_params)
 
+    if comment.save
+      redirect_to book_path(book), notice: "已新增留言"
+    else
+      redirect_to users_library_path, notice: "留言失敗，請重新嘗試"
+    end
+  end
+
+
+  private
+  def comment_params
+    params.require(:comment).permit(:stars, :content)
+  end
   
 end
