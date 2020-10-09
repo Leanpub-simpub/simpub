f0de46a6b92a0201fa0f62984d8b11ed01cf1ab4
Parker <ygfert@gmail.com>
2020-10-09 23:21:58 +0800

    更正 unwish 的路徑

--- app/javascript/controllers/wish_controller.js
+++ app/javascript/controllers/wish_controller.js
@@ -1,7 +1,6 @@
 import { Controller } from "stimulus";
 import axios from "axios";
 
-
 export default class extends Controller {
   static targets = [ "bubble" ];
   
@@ -13,16 +12,16 @@ export default class extends Controller {
     const username = this.data.get("username");
 
     axios.get(`/u/${username}/wishlist.json`)
-        .then(function(result) {
-          let wishCount = result.data.length;
-
-          if (wishCount !== 0) {
-            wishBubble.classList.add("bubble");
-            wishBubble.textContent = wishCount;
-          } else {
-            wishBubble.classList.remove("bubble");
-          }
-        })
-        .then(function(error) {});
+         .then(function(result) {
+           let wishCount = result.data.length;
+ 
+           if (wishCount !== 0) {
+             wishBubble.classList.add("bubble");
+             wishBubble.textContent = wishCount;
+           } else {
+             wishBubble.classList.remove("bubble");
+           }
+         })
+         .then(function(error) {});
   }
 }
