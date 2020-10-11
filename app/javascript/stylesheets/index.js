37da15a64efa0c44ea3e0d647eed9f7c9594782e
jean <macyjeanma@gmail.com>
2020-10-11 16:20:58 +0800

    frontend stylesheets

--- app/javascript/stylesheets/index.js
+++ app/javascript/stylesheets/index.js
@@ -6,6 +6,7 @@ import "./cart_review.scss";
 import "./typed.scss";
 import "./wishlist.scss";
 import "./modal.scss";
+import "./profiles_show.scss";
 
 import "./books";
 import "./home";
@@ -13,3 +14,4 @@ import "./layout";
 import "./shared";
 import "./text_editor";
 import "./users";
+
