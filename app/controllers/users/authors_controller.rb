9e66016ec31341dad54ca16aea28face3cc98617
Ted Chen <teddy70642000@gmail.com>
2020-10-14 22:16:49 +0800

    未登入時點選 my writings 轉址到登入，未成為作者時轉址到create a book

--- app/controllers/users/authors_controller.rb
+++ app/controllers/users/authors_controller.rb
@@ -3,6 +3,10 @@ class Users::AuthorsController < ApplicationController
   def show
     if not user_signed_in?
       redirect_to new_user_session_path, alert: 'You need to sign in or sign up before continuing.'
+    else
+      if not current_user.as_author
+        redirect_to new_book_path, alert: 'Create your books.'
+      end
     end
   end
 
